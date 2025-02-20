a1f09d4443907a2e5e4f586ae473ad3a895997e6
sari705 <sari125705@gmail.com>
2025-02-20 20:06:55 +0200

    עדכון שרות מוצרים

--- TinyDreams/src/api/productService.js
+++ TinyDreams/src/api/productService.js
@@ -1,15 +1,30 @@
-import {axios} from "axios";
+import { axios } from "axios";
 
-let baseUrl = "https://nodeproject-gd82.onrender.com";
+let baseUrl = "https://nodeproject-gd82.onrender.com/api/product";
 
-export function getAllProducts(index){
-    return axios.get(baseUrl + "?limit=15&page=" + index);
+export function getAllProducts(index) {
+    return axios.get(`${baseUrl}?limit=15&page=${index}`);
 }
 
-export function getProduct(){
+export function getProduct(productId) {
+    return axios.get(`${baseUrl}/${productId}`)
+}
+
+export function getTotalPages() {
     return axios.get(`${baseUrl}/totalPages?limit=15`)
 }
-export function 
+
+export function addProduct(product) {
+    return axios.post(baseUrl, product)
+}
+
+export function deleteProduct(productId) { 
+    return axios.delete(`${baseUrl}/${productId}`)
+}
+
+export function updateProduct(product) {
+    return axios.put(`${baseUrl}/${product._id}`, product)
+}
 
 
 

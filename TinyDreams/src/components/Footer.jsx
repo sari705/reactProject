658e9ca63ae8cba19883da1572ce44852f3e0b0c
sari705 <sari125705@gmail.com>
2025-03-24 17:20:15 +0200

    add contact button in footer

--- TinyDreams/src/components/Footer.jsx
+++ TinyDreams/src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Typography, Box, Button } from '@mui/material';
+import { Email } from '@mui/icons-material';
+
 
 const Footer = () => {
     const [isVisible, setIsVisible] = useState(false);
+    const emailAddress = "tinydreams46@gmail.com"
 
     useEffect(() => {
         const handleScroll = () => {
@@ -31,6 +34,26 @@ const Footer = () => {
             <Container maxWidth={false} sx={{ width: '100%' }}>
                 <Typography variant="body1">&copy; {new Date().getFullYear()} כל הזכויות שמורות</Typography>
             </Container>
+            <a
+                href={`https://mail.google.com/mail/?view=cm&fs=1&to=${emailAddress}&su=יצירת קשר&body=שלום, אני רוצה לדבר על...`}
+                style={{ textDecoration: 'none' }}
+                target='_blank'
+            >
+                <Button
+                    variant="contained"
+                    color="primary"
+                    // endIcon={<Email />}
+                    sx={{
+                        borderRadius: '20px',
+                        padding: '10px 20px',
+                        textTransform: 'none',
+                        marginTop: '10px',
+                        width:"20%",
+                        color: "#590202",
+                    }}
+                > ✉︎ יצירת קשר   
+                </Button>
+            </a>
         </Box>
     );
 };
